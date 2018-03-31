9a9b7189c25225d71ad5e8049cffc96e400d565b
autodocs <autodocs>
2018-03-31 22:05:19 +0000

    build based on 16e002d

---
+++ latest/search_index.js
@@ -0,0 +1,35 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#SchwarzChristoffel-1",
+    "page": "Home",
+    "title": "SchwarzChristoffel",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "index.html#About-the-package-1",
+    "page": "Home",
+    "title": "About the package",
+    "category": "section",
+    "text": "The purpose of this package is to enable easy construction and evaluation of the mapping from the region inside or outside the unit circle to the exterior of a closed polygon."
+},
+
+{
+    "location": "index.html#Installation-1",
+    "page": "Home",
+    "title": "Installation",
+    "category": "section",
+    "text": "This package requires Julia 0.6- and above. It is not a registered package, so it should be installed with:julia> Pkg.clone(\"git@github.com:jdeldre/SchwarzChristoffel.jl.git\")Since it is still under heavy development, you should runjulia> Pkg.test(\"SchwarzChristoffel\") # might take some timeto make sure things are working as intended andjulia> Pkg.update()to get the most recent version of the library and its dependencies.Examples can be found in the documentation and the Jupyter notebooks."
+},
+
+]}
