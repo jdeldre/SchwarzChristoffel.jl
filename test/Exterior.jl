3ded369bfa48217f79d52ba3c466d03a86f52a47
Jeff Eldredge <jdeldre@g.ucla.edu>
2019-01-02 19:45:35 -0800

    Fixed test isapprox to \approx

--- test/Exterior.jl
+++ test/Exterior.jl
@@ -126,12 +126,12 @@ end
 
   zeta = [1.0+3.0im,-2.0-2.0im,0.0+1.1im]
   z = m(zeta)
-  @test isapprox(z,Complex128[0.269784+0.768849im,
+  @test z ≈ ComplexF64[0.269784+0.768849im,
                       -0.616452-0.470251im,
-                      -0.0310697+0.138951im];atol=1e-5)
+                      -0.0310697+0.138951im]
 
   m⁻¹ = SchwarzChristoffel.InverseMap(m)
-  @test isapprox(m⁻¹(m(zeta)),zeta;atol=eps())
+  @test m⁻¹(m(zeta)) ≈ zeta
 
 
 end
