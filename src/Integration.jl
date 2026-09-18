390622ffb934b3819c372d35e108d28c7c432cf9
David White <white.david.w@gmail.com>
2026-09-18 08:55:11 -0400

    Getting InexactError: Int64(-0.9602898565106835) from Integration.jl, due to FastGaussQuadrature taking an integer fast path inappropriately.  Replacing integer 0 with float 0.0 allows tests to run.

--- src/Integration.jl
+++ src/Integration.jl
@@ -11,9 +11,9 @@ function qdata(beta::Vector{Float64},nqpts::Int64)
   qnode = zeros(nqpts,n+1)
   qwght = zeros(nqpts,n+1)
   for j = findall(beta.>-1)
-    qnode[:,j],qwght[:,j] = gaussjacobi(nqpts,0,beta[j])
+    qnode[:,j],qwght[:,j] = gaussjacobi(nqpts,0.0,beta[j])
   end
-  qnode[:,n+1],qwght[:,n+1] = gaussjacobi(nqpts,0,0)
+  qnode[:,n+1],qwght[:,n+1] = gaussjacobi(nqpts,0.0,0.0)
   return qnode, qwght
 
 
